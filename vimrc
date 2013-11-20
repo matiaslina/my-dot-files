bfab2765996f7a5fca6496cdc22e7b7ea00fe739
Matias Linares <mlm0111@famaf.unc.edu.ar>
2013-11-20 14:27:12 -0300

    Updated

---
+++ vimrc
@@ -0,0 +1,82 @@
+filetype plugin on
+filetype indent on
+
+set number
+
+set autoread 
+
+
+set backspace=eol,start,indent
+set whichwrap+=<,>,h,l
+
+set noerrorbells
+set novisualbell
+
+syntax enable
+
+call pathogen#infect()
+
+if has("gui_running")
+	set guioptions-=T
+
+    " Menu
+    set guioptions-=m
+    " Scrolls
+    set guioptions-=l
+    set guioptions-=r
+    set guifont=Fira\ Mono\ 9
+    colo molokai
+endif
+
+set nobackup
+set nowb
+set noswapfile
+
+" Indent
+set expandtab
+set smarttab
+set shiftwidth=4
+set tabstop=4
+set softtabstop=4
+set ai
+set si
+set wrap
+
+set showcmd
+"set showmode showcmd
+"set laststatus=2
+"setglobal cmdheight=1
+
+nmap <F1> <Esc>
+nmap <F2> :NERDTreeToggle <cr>
+nmap <F3> :TlistToggle <cr>
+
+au BufRead,BufNewFile *.go set filetype=go
+
+" No More Arrows
+map <up> <nop>
+map <down> <nop>
+map <left> <nop>
+map <right> <nop>
+
+
+" esto es para el haskellmode
+let g:haddock_browser="/usr/bin/firefox"
+au BufEnter *.hs compiler ghc
+
+" Mapeamos para usar una funcion con el f4
+nmap <F4> :GHCi <C-R><C-W> 
+
+" Mostrar menu
+let mapleader=","
+let g:mapleader=","
+
+function Show_menu()
+    if &guioptions =~# 'm'
+        set guioptions-=m
+    else
+        set guioptions+=m
+    endif
+endfunction
+
+nmap <Leader>A :call Show_menu() <cr>
---
+++ xmonad/xmonad.hs
@@ -0,0 +1,73 @@
+import XMonad
+import XMonad.Util.EZConfig
+import XMonad.Util.Run
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import System.IO
+
+import XMonad.Layout.Spacing
+
+-- Trying shell
+import XMonad.Prompt
+import XMonad.Prompt.Shell
+
+myLayout = tiled ||| Mirror tiled ||| Full
+    where
+        --default tiling algoritm
+        tiled = spacing 2 $ Tall nmaster delta ratio
+
+        -- default number of windows in master pane
+        nmaster = 1
+
+        -- Default proportion of screen occupied
+        ratio = 7/10
+
+        -- persent of screen to increment by when resizin panes
+        delta = 3/100
+
+-- shell prompt config
+mySP = defaultXPConfig
+       {
+           font     = "xft:Fira Mono:pixelsize=10",
+           bgColor  = "#000000",
+           fgHLight = "#f8f8f8",
+           bgHLight = "#125ba6",
+           borderColor = "#125ba6",
+           promptBorderWidth = 1,
+           position = Bottom,
+           height = 15,
+           -- Supongo que sera algo como el comando por
+           -- defecto. 
+           defaultText = []
+       }
+
+main = do
+    xmproc <- spawnPipe "xmobar"
+    spawn trayerScript
+    spawn "nm-applet"
+    spawn "nitrogen --restore"
+    xmonad $ defaultConfig
+        { terminal = "gnome-terminal"
+        , modMask = mod4Mask
+        , borderWidth = 1
+        , normalBorderColor = "#000"
+        , focusedBorderColor = "#125ba6"
+        , manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ myLayout
+        --, layoutHook = avoidStruts $ layoutHook defaultConfig
+        , logHook = dynamicLogWithPP $ xmobarPP
+            { ppOutput = hPutStrLn xmproc
+            , ppTitle = xmobarColor "green" "" . shorten 50
+            , ppLayout = const "" 
+            }
+        } `additionalKeys` [
+            -- Faltaria instalarlo :p pero por ahora va bien.
+            ((mod4Mask, xK_o), spawn "firefox"),
+            ((mod4Mask .|. shiftMask, xK_o), spawn "thunderbird"),
+            ((mod4Mask, xK_p), shellPrompt mySP),
+
+            -- shell
+            ((mod4Mask .|. controlMask, xK_x), shellPrompt defaultXPConfig)
+        ]
+
+trayerScript = "trayer --align right --edge top --widthtype pixel --width 76 --height 8 --SetDockType true --SetPartialStrut true  --transparent true --alpha 0 --tint 0x000000"
